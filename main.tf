b33e4120b337d05b4aac62da0e9dd5287eaf111a
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-06 15:50:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Prometheus" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_prometheus.id]
 #   instance_type = "t3.medium"
-
+ 
   # need more for terraform     
 #   root_block_device { 
 #     volume_size = 50
